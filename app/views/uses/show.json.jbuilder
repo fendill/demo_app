31616c7317e1b3bfe6c8b71769f00d0772532300
Isao Miyao <isao@Isao-MacBook-Pro.local>
2015-06-23 23:59:04 +0900

    Finish demo app

---
+++ app/views/uses/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @use, :name, :email, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
-Rails.application.routes.draw do
+DemoApp::Application.routes.draw do
+  resources :microposts
+
+  resources :uses
+
   resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
